2457651ba8605619837a4b48de72237089e8f8f3
Bipin Thapa <tailung12345678910@gmail.com>
2024-10-27 10:31:55 +0545

    on the way

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function getmeme()
 
 document.addEventListener("DOMContentLoaded", getmeme());
 
+
+function getjoke ()
+{
+   fetch("https://official-joke-api.appspot.com/random_joke")
+  .then(Response => Response.json())
+  .then(data => {
+    const  setup = document.querySelector("#setup");
+    const punchline = document.querySelector("#punchline");
+    setup.innerHTML = data.setup;
+    punchline.innerHTML = data.punchline;
+    // punchline.classList.toggle('hidden')
+  })
+  .catch(error => console.error(error));
+}
+
+document.addEventListener("DOMContentLoaded",getjoke());
+
+document.addEventListener("DOMContentLoaded",function(){
+document.getElementById('reveal-button').addEventListener('click', function() {
+document.getElementById('punchline').classList.toggle('hidden');});
+});
