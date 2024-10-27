c22c97b18fac32f4a00dfd82a9f1c90e55d55373
Bipin Thapa <tailung12345678910@gmail.com>
2024-10-27 19:51:58 +0545

    completed

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
+
+const meme = document.querySelector("#meme-image");
+const  setup = document.querySelector("#setup");
+const punchline = document.querySelector("#punchline");
+const  quote = document.querySelector("#quote");
+const quotee = document.querySelector("#quotee");
+
+
 document.addEventListener("DOMContentLoaded",() => {
   getjoke();
   getmeme();
+  getquote();
 })
 
 
 function getmeme()
 {
-  fetch("https://api.apileague.com/retrieve-random-meme?api-key=455d9344f1364f71837644c7fd8dcf0b")
+
+  // fetch("https://api.apileague.com/retrieve-random-meme?api-key=455d9344f1364f71837644c7fd8dcf0b")
+  fetch("https://meme-api.com/gimme")
   .then(Response => Response.json())
   .then(data => 
     {
-      document.querySelector("#meme-image").src = data.url;
+      meme.src = data.url;
     })
   .catch(error => console.error(error));
 
@@ -20,8 +31,6 @@ function getmeme()
 
 function getjoke ()
 {
-  const  setup = document.querySelector("#setup");
-  const punchline = document.querySelector("#punchline");
 
    fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw")
   .then(Response => Response.json())
@@ -29,16 +38,27 @@ function getjoke ()
     if (data.type === "twopart") {
       setup.innerHTML = data.setup;
       punchline.innerHTML = data.delivery;
-      document.getElementById('punchline').classList.remove('hidden');
+      punchline.classList.remove('hidden');
  
     }
     else {
 
       setup.innerHTML = data.joke;
-      document.getElementById('punchline').classList.add('hidden');
+      punchline.classList.add('hidden');
 
 
     }
   })
+  .catch(error => console.error(error));
+}
+
+function getquote() {
+  fetch("https://quotes-api-self.vercel.app/quote")
+  .then(response => response.json())
+  .then(data => {
+    quote.innerHTML = data.quote;
+    quotee.innerHTML = `- ${data.author}`;
+  }
+  )
   .catch(error => console.error(error));
 }
